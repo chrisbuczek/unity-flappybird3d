d226d4f94e569736f2e1f995f98c183ea3040890
chrisbuczek <krzysztofbuczek@live.com>
2025-09-25 02:44:54 +0200

    add test script

--- Assets/Scripts/NewMonoBehaviourScript.cs
+++ Assets/Scripts/NewMonoBehaviourScript.cs
@@ -5,7 +5,7 @@ public class NewMonoBehaviourScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // this is a test
     }
 
     // Update is called once per frame
